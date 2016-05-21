a38c89622cfb2edf43e11596d7d36612635403ee
Christoph Roeder <brightdroid@users.noreply.github.com>
2016-05-21 12:21:41 +0200

    Fixed indentation

--- autocompletion/tldr.bash
+++ autocompletion/tldr.bash
@@ -2,7 +2,7 @@ function _tldr_autocomplete {
     sheets=$(tldr -l -1)
     COMPREPLY=()
     if [ $COMP_CWORD = 1 ]; then
-	COMPREPLY=(`compgen -W "$sheets" -- $2`)
+        COMPREPLY=(`compgen -W "$sheets" -- $2`)
     fi
 }
 
