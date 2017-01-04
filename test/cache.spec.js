a40fa2e225bec52d1560549d40761c8aefef5ec0
Agniva De Sarker <agnivade@yahoo.co.in>
2017-01-04 10:17:23 +0530

    update cache.js

--- test/cache.spec.js
+++ test/cache.spec.js
@@ -6,10 +6,15 @@ var index = require('../lib/index');
 var platform = require('../lib/platform');
 
 
-describe('Cache', function() {
+describe('Cache', () => {
 
-  it('should return a positive number on lastUpdate', function() {
-    cache.lastUpdate.should.be.aboveOrEqual(0);
+  it('should return a positive number on lastUpdate', (done) => {
+    cache.lastUpdated((err, stats) => {
+      should.not.exist(err);
+      should.exist(stats);
+      stats.mtime.should.be.aboveOrEqual(0);
+      done();
+    });
   });
 
   describe('getPage()', function() {
@@ -45,38 +50,62 @@ describe('Cache', function() {
       index.getShortIndex.restore();
     });
 
-    it('should return page contents for ls', function() {
-      sinon.stub(fs, 'readFileSync').returns('# ls\n> ls page');
+    it('should return page contents for ls', (done) => {
+      sinon.stub(fs, 'readFile', (path, encoding, cb) => {
+        return cb(null, '# ls\n> ls page');
+      });
       sinon.stub(platform, 'getPreferredPlatformFolder').returns('osx');
       sinon.stub(index, 'findPlatform').returns('osx');
-      cache.getPage('ls').should.startWith('# ls');
-      fs.readFileSync.restore();
-      platform.getPreferredPlatformFolder.restore();
-      index.findPlatform.restore();
+      cache.getPage('ls', (err, content) => {
+        should.not.exist(err);
+        should.exist(content);
+        content.should.startWith('# ls');
+        fs.readFile.restore();
+        platform.getPreferredPlatformFolder.restore();
+        index.findPlatform.restore();
+        done();
+      });
     });
 
-    it('should return empty contents for svcs on OSX', function() {
-      sinon.stub(fs, 'readFileSync').returns('# svcs\n> svcs');
+    it('should return empty contents for svcs on OSX', (done) =>{
+      sinon.stub(fs, 'readFile', (path, encoding, cb) => {
+        return cb(null, '# svcs\n> svcs');
+      });
       sinon.stub(platform, 'getPreferredPlatformFolder').returns('osx');
       sinon.stub(index, 'findPlatform').returns(null);
-      should.not.exist(cache.getPage('svcs'));
-      platform.getPreferredPlatformFolder.restore();
-      fs.readFileSync.restore();
-      index.findPlatform.restore();
+      cache.getPage('svc', (err, content) => {
+        should.not.exist(err);
+        should.not.exist(content);
+        fs.readFile.restore();
+        platform.getPreferredPlatformFolder.restore();
+        index.findPlatform.restore();
+        done();
+      });
     });
 
-    it('should return page contents for svcs on SunOS', function() {
-      sinon.stub(fs, 'readFileSync').returns('# svcs\n> svcs');
+    it('should return page contents for svcs on SunOS', (done) => {
+      sinon.stub(fs, 'readFile', (path, encoding, cb) => {
+        return cb(null, '# svcs\n> svcs');
+      });
       sinon.stub(platform, 'getPreferredPlatformFolder').returns('sunos');
       sinon.stub(index, 'findPlatform').returns('svcs');
-      cache.getPage('svcs').should.startWith('# svcs');
-      platform.getPreferredPlatformFolder.restore();
-      fs.readFileSync.restore();
-      index.findPlatform.restore();
+      cache.getPage('svcs', (err, content) => {
+        should.not.exist(err);
+        should.exist(content);
+        content.should.startWith('# svcs');
+        fs.readFile.restore();
+        platform.getPreferredPlatformFolder.restore();
+        index.findPlatform.restore();
+        done();
+      });
     });
 
-    it('should return empty contents for non-existing page', function() {
-      should.not.exist(cache.getPage('qwerty'));
+    it('should return empty contents for non-existing page', (done) => {
+      cache.getPage('qwerty', (err, content) => {
+        should.not.exist(err);
+        should.not.exist(content);
+        done();
+      });
     });
   });
 
