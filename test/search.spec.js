59c8b71e8322afa94453459bbd3b8c06dbf737fb
Rohitt Vashishtha <aero31aero@gmail.com>
2017-11-15 05:36:38 +0530

    search: Update docs and README. Create dummy test file.

---
+++ test/search.spec.js
@@ -0,0 +1,13 @@
+// 'use strict';
+
+// const search = require('../lib/search');
+// const should = require('should');
+// const sinon = require('sinon');
+
+// TODO write tests for private functions in the search module.
+// describe('Search', ()=> {
+// 	describe('readCorpus')
+// 	it('should read corpus into memory', done => {
+
+// 	});
+// });
