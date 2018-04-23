37937394fdc43e072540cf067a345672ab26455d
Dario Vladović <d.vladimyr@gmail.com>
2018-04-23 15:09:45 +0200

    Use random temp folder for remote zip extraction (#210)
    * Use random temp folder for remote zip extraction
    
    - generating random folder inside `/tmp/tldr/`
    - removing temp folders after cache has been updated

--- test/cache.spec.js
+++ test/cache.spec.js
@@ -5,6 +5,7 @@ const should = require('should');
 const sinon = require('sinon');
 const fs = require('fs-extra');
 const index = require('../lib/index');
+const remote = require('../lib/remote');
 const platform = require('../lib/platform');
 
 
@@ -21,6 +22,50 @@ describe('Cache', () => {
         should.exist(stats);
         stats.mtime.should.be.aboveOrEqual(0);
       });
+  });
+
+  describe('update()', () => {
+    beforeEach(() => {
+      sinon.spy(fs, 'ensureDir');
+      sinon.spy(fs, 'remove');
+      sinon.stub(fs, 'copy').resolves();
+      sinon.stub(remote, 'download').resolves();
+      sinon.stub(index, 'rebuildPagesIndex').resolves();
+    });
+
+    it('should use randomly created temp folder', () => {
+      const count = 16;
+      return Promise.all(Array.from({ length: count }).map(() => {
+        return cache.update();
+      })).then(() => {
+        let calls = fs.ensureDir.getCalls().filter((call) => {
+          return !call.calledWith(cache.CACHE_FOLDER);
+        });
+        calls.should.have.length(count);
+        let tempFolders = calls.map((call) => {
+          return call.args[0];
+        });
+        tempFolders.should.have.length(new Set(tempFolders).size);
+      });
+    });
+
+    it('should remove temp folder after cache gets updated', () => {
+      return cache.update().then(() => {
+        let createFolder = fs.ensureDir.getCalls().find((call) => {
+          return !call.calledWith(cache.CACHE_FOLDER);
+        });
+        let removeFolder = fs.remove.getCall(0);
+        removeFolder.args[0].should.be.equal(createFolder.args[0]);
+      });
+    });
+
+    afterEach(() => {
+      fs.ensureDir.restore();
+      fs.remove.restore();
+      fs.copy.restore();
+      remote.download.restore();
+      index.rebuildPagesIndex.restore();
+    });
   });
 
   describe('getPage()', () => {
