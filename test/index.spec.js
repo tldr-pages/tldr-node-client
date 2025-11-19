59b9d393b066806b71c7d301eee2fbeb96a23521
Seth Falco <seth@falco.fun>
2025-11-19 22:00:09 +0000

    chore: migrate to node.js native test runner (#517)

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,11 @@
 'use strict';
 
+const assert = require('node:assert/strict')
+const { describe, it, beforeEach } = require('node:test');
 const fs = require('fs-extra');
 const path = require('path');
 const index = require('../lib/index');
 const utils = require('../lib/utils');
-const sinon = require('sinon');
-const should = require('should');
 
 const pages = [
   ['/index.json'],
@@ -35,212 +35,225 @@ const pages = [
 });
 
 describe('Index building', () => {
-  beforeEach(() => {
-    sinon.stub(fs, 'readJson').rejects('dummy error');
-    sinon.stub(fs, 'writeJson').resolves('');
-    return index.rebuildPagesIndex();
-  });
+  /** @param {import('node:test').TestContext} t */
+  function mockFns(t) {
+    t.mock.method(fs, 'readJSON', () => Promise.reject('dummy error'));
+    t.mock.method(fs, 'writeJson', () => Promise.resolve(''));
+  }
 
   describe('failure', () => {
-    before(() => {
-      sinon.stub(utils, 'walk').rejects('dummy error');
-    });
+    it('shortIndex should not be created', async (t) => {
+      mockFns(t);
+      t.mock.method(utils, 'walk', () => Promise.reject('dummy error'));
+      await index.rebuildPagesIndex();
 
-    it('shortIndex should not be created', () => {
-      return index.hasPage('cp').should.be.false() &&
-        index.hasPage('dummy').should.be.false();
+      assert.equal(index.hasPage('cp'), false);
+      assert.equal(index.hasPage('dummy'), false);
     });
   });
 
   describe('success', () => {
-    before(() => {
-      sinon.stub(utils, 'walk').resolves(pages);
-    });
+    it('correct shortIndex should be created', async (t) => {
+      mockFns(t);
+      t.mock.method(utils, 'walk', () => Promise.resolve(pages));
+      await index.rebuildPagesIndex();
 
-    it('correct shortIndex should be created', () => {
-      return index.hasPage('cp').should.be.true() &&
-        index.hasPage('dummy').should.be.false();
+      assert.equal(index.hasPage('cp'), true);
+      assert.equal(index.hasPage('dummy'), false);
     });
-  });
-
-  afterEach(() => {
-    /** @type {sinon.SinonSpy} */ (utils.walk).restore();
-    fs.readJson.restore();
-    fs.writeJson.restore();
   });
 });
 
 describe('Index', () => {
+  /** @param {import('node:test').TestContext} t */
+  function mockFns(t) {
+    t.mock.method(utils, 'walk', () => Promise.resolve(pages));
+    t.mock.method(fs, 'readJSON', () => Promise.reject('dummy error'));
+    t.mock.method(fs, 'writeJson', () => Promise.resolve(''));
+  }
+
   beforeEach(() => {
     index.clearRuntimeIndex();
-    sinon.stub(utils, 'walk').resolves(pages);
-    sinon.stub(fs, 'readJson').rejects('dummy error');
-    sinon.stub(fs, 'writeJson').resolves('');
-  });
-
-  afterEach(() => {
-    /** @type {sinon.SinonSpy} */ (utils.walk).restore();
-    fs.readJson.restore();
-    fs.writeJson.restore();
   });
 
   describe('findPage()', () => {
-    it('should find Linux platform for apk command for Chinese', () => {
+    it('should find Linux platform for apk command for Chinese', (t) => {
+      mockFns(t);
       return index.findPage('apk', 'linux', 'zh')
         .then((folder) => {
-          should.equal(folder, path.join('pages.zh', 'linux'));
+          assert.equal(folder, path.join('pages.zh', 'linux'));
         });
     });
 
-    it('should find platform android for pkg command for English', () => {
+    it('should find platform android for pkg command for English', (t) => {
+      mockFns(t);
       return index.findPage('pkg', 'android', 'en')
         .then((folder) => {
-          should.equal(folder, path.join('pages', 'android'));
+          assert.equal(folder, path.join('pages', 'android'));
         });
     });
 
-    it('should find platform freebsd for pkg command for English', () => {
+    it('should find platform freebsd for pkg command for English', (t) => {
+      mockFns(t);
       return index.findPage('pkg', 'freebsd', 'en')
         .then((folder) => {
-          should.equal(folder, path.join('pages', 'freebsd'));
+          assert.equal(folder, path.join('pages', 'freebsd'));
         });
     });
 
-    it('should find platform openbsd for pkg command for English', () => {
+    it('should find platform openbsd for pkg command for English', (t) => {
+      mockFns(t);
       return index.findPage('pkg', 'openbsd', 'en')
         .then((folder) => {
-          should.equal(folder, path.join('pages', 'openbsd'));
+          assert.equal(folder, path.join('pages', 'openbsd'));
         });
     });
 
-    it('should find platform netbsd for pkgin command for English', () => {
+    it('should find platform netbsd for pkgin command for English', (t) => {
+      mockFns(t);
       return index.findPage('pkgin', 'netbsd', 'en')
         .then((folder) => {
-          should.equal(folder, path.join('pages', 'netbsd'));
+          assert.equal(folder, path.join('pages', 'netbsd'));
         });
     });
 
-    it('should find Linux platform for apk command for Chinese given Windows', () => {
+    it('should find Linux platform for apk command for Chinese given Windows', (t) => {
+      mockFns(t);
       return index.findPage('apk', 'windows', 'zh')
         .then((folder) => {
-          should.equal(folder, path.join('pages.zh', 'linux'));
+          assert.equal(folder, path.join('pages.zh', 'linux'));
         });
     });
 
-    it('should find Linux platform for dd command', () => {
+    it('should find Linux platform for dd command', (t) => {
+      mockFns(t);
       return index.findPage('dd', 'linux', 'en')
         .then((folder) => {
-          should.equal(folder, path.join('pages', 'linux'));
+          assert.equal(folder, path.join('pages', 'linux'));
         });
     });
 
-    it('should find platform common for cp command for English', () => {
+    it('should find platform common for cp command for English', (t) => {
+      mockFns(t);
       return index.findPage('cp', 'linux', 'en')
         .then((folder) => {
-          should.equal(folder, path.join('pages', 'common'));
+          assert.equal(folder, path.join('pages', 'common'));
         });
     });
 
-    it('should find platform common for cp command for Tamil', () => {
+    it('should find platform common for cp command for Tamil', (t) => {
+      mockFns(t);
       return index.findPage('cp', 'linux', 'ta')
         .then((folder) => {
-          should.equal(folder, path.join('pages.ta', 'common'));
+          assert.equal(folder, path.join('pages.ta', 'common'));
         });
     });
 
-    it('should find platform common for cp command for Italian', () => {
+    it('should find platform common for cp command for Italian', (t) => {
+      mockFns(t);
       return index.findPage('cp', 'linux', 'it')
         .then((folder) => {
-          should.equal(folder, path.join('pages.it', 'common'));
+          assert.equal(folder, path.join('pages.it', 'common'));
         });
     });
 
-    it('should find platform common for cp command for Italian given Windows', () => {
+    it('should find platform common for cp command for Italian given Windows', (t) => {
+      mockFns(t);
       return index.findPage('cp', 'windows', 'it')
         .then((folder) => {
-          should.equal(folder, path.join('pages.it', 'common'));
+          assert.equal(folder, path.join('pages.it', 'common'));
         });
     });
 
-    it('should find platform common for ls command for Italian', () => {
+    it('should find platform common for ls command for Italian', (t) => {
+      mockFns(t);
       return index.findPage('ls', 'linux', 'it')
         .then((folder) => {
-          should.equal(folder, path.join('pages', 'common'));
+          assert.equal(folder, path.join('pages', 'common'));
         });
     });
 
 
-    it('should find platform common for cp command for Italian given common platform', () => {
+    it('should find platform common for cp command for Italian given common platform', (t) => {
+      mockFns(t);
       return index.findPage('cp', 'common', 'it')
         .then((folder) => {
-          should.equal(folder, path.join('pages.it', 'common'));
+          assert.equal(folder, path.join('pages.it', 'common'));
         });
     });
 
-    it('should find platform common for cp command for English given a bad language', () => {
+    it('should find platform common for cp command for English given a bad language', (t) => {
+      mockFns(t);
       return index.findPage('cp', 'linux', 'notexist')
         .then((folder) => {
-          should.equal(folder, path.join('pages', 'common'));
+          assert.equal(folder, path.join('pages', 'common'));
         });
     });
 
-    it('should find platform for svcs command on Linux', () => {
+    it('should find platform for svcs command on Linux', (t) => {
+      mockFns(t);
       return index.findPage('svcs', 'linux', 'en')
         .then((folder) => {
-          should.equal(folder, path.join('pages', 'sunos'));
+          assert.equal(folder, path.join('pages', 'sunos'));
         });
     });
 
-    it('should not find platform for non-existing command', () => {
+    it('should not find platform for non-existing command', (t) => {
+      mockFns(t);
       return index.findPage('qwerty', 'linux', 'en')
         .then((folder) => {
-          should.not.exist(folder);
+          assert.equal(!!folder, false);
         });
     });
   });
 
-  it('should return the correct list of all pages', () => {
+  it('should return the correct list of all pages', (t) => {
+    mockFns(t);
     return index.commands()
       .then((commands) => {
-        should.deepEqual(commands, [
+        assert.deepEqual(commands, [
           'apk', 'cp', 'dd', 'du', 'git', 'ln', 'ls', 'pkg', 'pkgin', 'svcs', 'top'
-        ]);
+        ])
       });
   });
 
   describe('commandsFor()', () => {
-    it('should return the correct list of pages for Linux', () => {
+    it('should return the correct list of pages for Linux', (t) => {
+      mockFns(t);
       return index.commandsFor('linux')
         .then((commands) => {
-          should.deepEqual(commands, [
+          assert.deepEqual(commands, [
             'apk', 'cp', 'dd', 'du', 'git', 'ln', 'ls', 'top'
           ]);
         });
     });
 
-    it('should return the correct list of pages for OSX', () => {
+    it('should return the correct list of pages for OSX', (t) => {
+      mockFns(t);
       return index.commandsFor('osx')
         .then((commands) => {
-          should.deepEqual(commands, [
+          assert.deepEqual(commands, [
             'cp', 'dd', 'du', 'git', 'ln', 'ls', 'top'
           ]);
         });
     });
 
-    it('should return the correct list of pages for SunOS', () => {
+    it('should return the correct list of pages for SunOS', (t) => {
+      mockFns(t);
       return index.commandsFor('sunos')
         .then((commands) => {
-          should.deepEqual(commands, [
+          assert.deepEqual(commands, [
             'cp', 'dd', 'du', 'git', 'ln', 'ls', 'svcs'
           ]);
         });
     });
   });
 
-  it('should return the correct short index on getShortIndex()', () => {
+  it('should return the correct short index on getShortIndex()', (t) => {
+    mockFns(t);
     return index.getShortIndex()
       .then((idx) => {
-        should.deepEqual(idx, {
+        assert.deepEqual(idx, {
           apk: { targets: [{ language: 'en', platform: 'linux' }, { language: 'zh', platform: 'linux' }] },
           cp: { targets: [{ language: 'en', platform: 'common' }, { language: 'it', platform: 'common' }, { language: 'ta', platform: 'common' }] },
           dd: { targets: [{ language: 'en', platform: 'linux' }, { language: 'en', platform: 'osx' }, { language: 'en', platform: 'sunos' }] },
--- test/remote.spec.js
+++ test/remote.spec.js
@@ -1,18 +1,16 @@
 'use strict';
 
+const assert = require('node:assert/strict')
+const { describe, it, afterEach } = require('node:test');
 const Cache = require('../lib/cache');
 const config = require('../lib/config');
-const sinon = require('sinon');
 const path = require('path');
 const fs = require('fs-extra');
 const index = require('../lib/index');
 const utils = require('../lib/utils');
-var assert = require('assert');
 
 describe('Remote', () => {
   describe('update()', () => {
-    const TIMEOUT_INTERVAL = 120000; // 2 min timeout for each test case.
-
     const testCases = [
       {
         description: 'No language specified',
@@ -42,52 +40,44 @@ describe('Remote', () => {
     ];
 
     testCases.forEach((testCase) => {
-      describe(`${testCase.description}`, () => {
+      describe(`${testCase.description}`, async () => {
+        /** @type {import('node:test').Mock<() => unknown>} */
+        let remove;
         let tempFolder;
 
-        beforeEach(() => {
-          sinon.spy(fs, 'ensureDir');
-          sinon.stub(fs, 'remove').resolves();
-          sinon.stub(fs, 'copy').resolves();
-          sinon.stub(utils, 'localeToLang').returns(testCase.LANG);
-          sinon.stub(index, 'rebuildPagesIndex').resolves();
+        await it('passes', async (t) => {
+          remove = t.mock.method(fs, 'remove', () => Promise.resolve());
+          t.mock.method(fs, 'ensureDir');
+          t.mock.method(fs, 'copy', () => Promise.resolve());
+          t.mock.method(utils, 'localeToLang', () => testCase.LANG);
+          t.mock.method(index, 'rebuildPagesIndex', () => Promise.resolve());
+
+          const cache = new Cache(config.get());
+          await cache.update();
+
+          let call = fs.ensureDir.mock.calls[0];
+          tempFolder = call.arguments[0];
+
+          // Get the actual cache folders created
+          const items = fs.readdirSync(tempFolder);
+
+          // Filter the items to get only the directories
+          const presentFolders = items.filter((item) => {
+            try {
+              return fs.statSync(path.join(tempFolder, item)).isDirectory();
+            } catch (err) {
+              return false;
+            }
+          });
+          assert.deepEqual(presentFolders, testCase.expectedFolders);
         });
 
-        it('passes', () => {
-          const cache = new Cache(config.get());
-          return cache.update().then(() => {
-            let call = fs.ensureDir.getCall(0);
-            tempFolder = call.args[0];
-
-            // Get the actual cache folders created
-            const items = fs.readdirSync(tempFolder);
-
-            // Filter the items to get only the directories
-            const presentFolders = items.filter((item) => {
-              try {
-                return fs.statSync(path.join(tempFolder, item)).isDirectory();
-              } catch (err) {
-                return false;
-              }
-            });
-            assert.deepEqual(presentFolders, testCase.expectedFolders);
-          }).catch((err) => {
-            throw err;
-          });
-        }).timeout(TIMEOUT_INTERVAL);
-
         afterEach(async () => {
-          // Clearing Spies & Stubs
-          fs.copy.restore();
-          fs.remove.restore();
-          fs.ensureDir.restore();
-          /** @type {sinon.SinonSpy} */ (utils.localeToLang).restore();
-          /** @type {sinon.SinonSpy} */ (index.rebuildPagesIndex).restore();
-
+          remove.mock.restore();
           await fs.remove(tempFolder);
-        }).timeout(TIMEOUT_INTERVAL);
+        });
 
-      }).timeout(TIMEOUT_INTERVAL);
+      });
     });
   });
 });
